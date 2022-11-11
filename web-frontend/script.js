c000b9ea23ec04e675db63be13c8c947dd9f9dbc
raz0229 <raja.zafar229@gmail.com>
2022-11-11 22:08:26 +0500

    added firebase

--- web-frontend/script.js
+++ web-frontend/script.js
@@ -1,3 +1,19 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js'
+import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js'
+
+      const firebaseConfig = {
+    apiKey: "AIzaSyB4hl9a1RPB_MmuqPm_zNmO49Y20qSf9e4",
+    authDomain: "stoked-sun-304807.firebaseapp.com",
+    projectId: "stoked-sun-304807",
+    storageBucket: "stoked-sun-304807.appspot.com",
+    messagingSenderId: "622344556987",
+    appId: "1:622344556987:web:2018a94371c8eeef90f49e",
+    measurementId: "G-9WXYEFVCQ0"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 let selected, roll;
 
 let year = document.querySelector(".cp-text #year");
