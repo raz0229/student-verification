d4c64729708ae89b42538ffb858cc12634a42dba
raz0229 <raja.zafar229@gmail.com>
2022-11-11 20:04:31 +0500

    error message

--- web-frontend/script.js
+++ web-frontend/script.js
@@ -1,2 +1,26 @@
+let selected, roll;
+
 let year = document.querySelector(".cp-text #year");
 year.textContent = new Date().getFullYear();
+
+document.querySelector('ul').addEventListener('click', (x) => {
+    selected = x.target.textContent
+    document.querySelector('span').innerText = selected;
+    document.querySelector('#touch').checked = false
+})
+
+document.querySelector('.submit').addEventListener('click', (e)=> {
+    e.preventDefault();
+    roll = document.querySelector('#roll').value
+    if (!selected)
+        document.querySelector('nav').style.borderColor = '#b00';
+    else if (!roll) {
+        document.querySelector('#roll').style.borderWidth = '1px';
+        document.querySelector('#roll').style.borderColor = '#b00';
+    }
+    else {
+        console.log(selected, roll)
+        document.querySelector('.error').style.display = 'block'
+    }
+    
+})
